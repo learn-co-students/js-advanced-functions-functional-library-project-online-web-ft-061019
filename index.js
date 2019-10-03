cacf5d3f2852647028b5b74217302a43be731d93
github username <github email address>
2019-10-03 10:46:38 -0400

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(src, fn) {
+      let newArr = []
+      for (let i = 0; i < src.length; i++){
+      const element = fn(src[i])
+      newArr.push(element)
+      }
+      return src
     },
 
-    map: function() {
-
+    map: function(src, fn) {
+      let newArr = []
+      for (let i = 0; i < src.length; i++) {
+          const element = fn(src[i])
+          newArr.push(element)
+       }
+       return newArr
     },
 
-    reduce: function() {
+    reduce: function(src, cb, starting=0) {
+      let s = (!!starting) ? starting : src[0]
+      let i = (!!starting) ? 0 : 1
+    
+      for (; i < src.length; i++) {
+        s = cb(src[i], s)
+      }
+    
+      return s;
 
     },
 
